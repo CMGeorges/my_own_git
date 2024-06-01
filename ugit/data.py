aa405ddfb452a93692687432371db736fbdfb1c5
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2024-06-01 11:44:57 -0400

    add cat-file

--- ugit/data.py
+++ ugit/data.py
@@ -12,4 +12,10 @@ def init():
 def hash_object(data):
     oid = hashlib.sha1(data).hexdigest()
     with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
         return oid
+
+
+def get_object (oid):
+     with open (f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+         return f.read ()
